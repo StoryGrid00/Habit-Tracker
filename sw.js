b11d8b28e6de9fa4bcc576013e56a3441b0ef9c3
StoryGrid00 <goodsdealer777@gmail.com>
2025-09-18 22:59:47 -0400

    C1

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ios-habit-pwa-v11';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-light.png','./logo-dark.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>r)));});
